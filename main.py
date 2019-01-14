001d9cce803b7895fe409bac2f802c33809370ae
GuruOfPython <vpeKJmPc@mail.com>
2019-01-13 17:52:55 -0800

    2nd commit

--- main.py
+++ main.py
@@ -1,63 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Chrome Options
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
 import os
 from lxml import html
+import requests
+import csv
 
-CHROME_OPTIONS = Options()
-# CHROME_OPTIONS.add_argument("--headless")
-# CHROME_OPTIONS.add_argument('--disable-gpu')  # Last I checked this was necessary.
-# CHROME_OPTIONS.add_argument('--disable-notifications')
-# prefs = {"profile.managed_default_content_settings.images": 2, 'disk-cache-size': 4096}
-# CHROME_OPTIONS.add_experimental_option('prefs', prefs)
-# CHROME_OPTIONS.add_argument('--ignore-certificate-errors')
-# CHROME_OPTIONS.add_argument("--test-type")
-# CHROME_OPTIONS.add_argument('--disable-infobars')
-# CHROME_OPTIONS.add_argument('--disable-extensions')
-# CHROME_OPTIONS.add_argument('--profile-directory=Default')
-# CHROME_OPTIONS.add_argument('--incognito')
-# CHROME_OPTIONS.add_argument('--disable-plugins-discovery')
-CHROME_OPTIONS.add_argument('--start-maximized')
+page_source = open("QS Graduate Employability Rankings 2019 _ Top Universities.html", "r", encoding="utf-8").read()
+tree = html.fromstring(page_source)
 
-driver = webdriver.Chrome(options=CHROME_OPTIONS, executable_path=os.getcwd() + r"\WebDriver\chromedriver.exe")
-driver.get(url="https://www.topuniversities.com/university-rankings/employability-rankings/2019")
-
-WebDriverWait(driver, 100).until(
-    EC.element_to_be_clickable(
-        (By.XPATH, '//span[@class="jcf-select-text"]/span[text()="25"]'))
-)
-
-option = driver.find_element_by_xpath('//span[@class="jcf-select-text"]/span[text()="25"]')
-option.click()
-
-all_select = driver.find_element_by_xpath('//span[text()="All"]')
-all_select.click()
-
-tree = html.fromstring(driver.page_source)
 
 rows = tree.xpath('//table[@id="qs-rankings"]/tbody/tr')
 for i, row in enumerate(rows):
     try:
-        rank = row.xpath('.//span[@class="rank "]/text()')
+        rank = row.xpath('.//span[@class="rank "]/text()')[0].strip()
     except:
         rank = ""
     try:
-        name = ""
+        name = row.xpath('td[@class=" uni"]//a[@class="title"]/text()')[0].strip()
     except:
         name = ""
     try:
-        location = ""
+        location = row.xpath('td[@class=" country"]/div/text()')[0].strip()
     except:
         location = ""
     try:
-        link = ""
+        link = row.xpath('td[@class=" uni"]//a[@class="title"]/@href')[0]
     except:
         link = ""
+
+    try:
+        r = requests.get(link)
+        tree = html.fromstring(r.text)
+        about = "\n".join([elm.strip() for elm in tree.xpath('//div[@class="field-profile-overview"]/p/text()') if elm.strip()])
+    except:
+        about = ""
+
